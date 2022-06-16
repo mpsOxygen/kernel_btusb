864542f60ec5bbd13b4f1d09bcdcfbf128a3ceca
Ravzan Valceanu <razvan.valceanu@metaminds.com>
2022-06-16 12:34:43 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Quick and dirty patch for Mediatek 7922 bluetooth on Fedora 36
+
+This is a quick and dirty script to enable bluetooth on Mediatek 7922 chips on Fedora 36 (but should work with any distro).
+
+All the bits are already there in order for the chip to work, but for a single entry in a list in btusb.c kernel module that say if you see this id 13d3:3568 use Mediatek firmware. Here is the patch to the kernel that fixes the problem: https://patchwork.kernel.org/project/linux-mediatek/patch/20220602092822.953521-1-aaron.ma@canonical.com/ But until this gets mainlined, every time I update the kernel I have to fix this all over again.
+
+So instead of building a new out of tree module every time I get a kernel update, I just run this script that binary patches the module and removes it's signing keys.
+You will need the following packages installed for it to work:
+  - xz
+  - binutils
+
+If you prefer to build the module, here is the bug report where I explain how to do it: https://bugzilla.redhat.com/show_bug.cgi?id=2091399
---
+++ patch_btusb.py
@@ -0,0 +1,50 @@
+#!/bin/env python
+
+import mmap
+import os
+
+
+patch = {
+        'path': '/lib/modules/' + os.uname().release + '/kernel/drivers/bluetooth/',
+        'file':      'btusb.ko.xz',
+		'offset': 	 0x0,
+		'original':  b'\x64',
+		'patched': 	 b'\x68',
+        'id_search': b'\xd3\x13\x64\x35',
+        'id_patch':  b'\xd3\x13\x68\x35'
+	}
+
+def bt_patch():
+    os.system('cp ' + patch['path'] + patch['file'] + ' .')
+    os.system('xz -d ' + patch['file'])
+    with open(patch['file'][:-3], 'r+b') as fis:
+        haystack = mmap.mmap(fis.fileno(), length=0, access=mmap.ACCESS_READ)
+        patch['offset'] = haystack.find(patch['id_search'])
+        if patch['offset'] == -1:
+            print('Did not find a match, exiting...')
+            return
+        print('Found offset: 0x%x' % patch['offset'])
+        fis.seek(patch['offset'])
+        data = fis.read(4)
+        print(data)
+        if data == patch['id_search']:
+            print('Ok! Patching...')
+        else:
+            print('Search bytes dont match, exiting!')
+            return
+        fis.seek(patch['offset'])
+        fis.write(patch['id_patch'])
+        fis.close()
+        print("Striping signing keys from module...")
+        os.system('strip -g ' + patch['file'][:-3])
+        print('Compressing back...')
+        os.system('xz ' + patch['file'][:-3])
+        print('Now all you have to do is: ')
+        print('  sudo cp ' + patch['file'] + ' ' + patch['path'])
+    return
+
+def main():
+    bt_patch()
+
+if __name__ == '__main__':
+    main()
